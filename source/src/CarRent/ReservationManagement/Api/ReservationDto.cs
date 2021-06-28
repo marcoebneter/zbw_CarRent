7f9114fd94bed8680e3b2864412a9cd2e223646c
marco <marco.ebneter@gmail.com>
2021-06-28 19:59:51 +0200

    added some files

---
+++ source/src/CarRent/ReservationManagement/Api/ReservationDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRent.ReservationManagement.Api
+{
+    public class ReservationDto
+    {
+    }
+}
