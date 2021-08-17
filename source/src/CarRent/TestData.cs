dcda576f2b2c77a74fc0a407e816557a933b3716
marco <marco.ebneter@gmail.com>
2021-08-17 19:10:16 +0200

    sonarcloud

--- source/src/CarRent/TestData.cs
+++ source/src/CarRent/TestData.cs
@@ -12,9 +12,19 @@ namespace CarRent
         public TestData(ModelBuilder modelBuilder)
         {
             var plz = new List<Plz>();
+            var cus = new List<Customer>();
 
             #region Plz
             plz.Add(new Plz() {city = "Appenzell", country = "Switzerland", plz = "9050", id = Guid.NewGuid()});
+            plz.Add(new Plz() {city = "Gossau", country = "Switzerland", plz = "9200", id = Guid.NewGuid()});
+            plz.Add(new Plz() { city = "Zürich", country = "Switzerland", plz = "8001", id = Guid.NewGuid() });
+            plz.Add(new Plz() { city = "Bern", country = "Switzerland", plz = "3001", id = Guid.NewGuid() });
+            plz.Add(new Plz() { city = "Basel", country = "Switzerland", plz = "4001", id = Guid.NewGuid() });
+            plz.Add(new Plz() { city = "Uri", country = "Switzerland", plz = "6460", id = Guid.NewGuid() });
+            #endregion
+
+            #region Customer
+
             #endregion
 
             foreach (Plz entity in plz)
